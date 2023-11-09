a3ffb19bfe3db2bf016008dce50fdeae922d61b9
Serking de Orayom <kingdeorayom@gmail.com>
2023-11-09 20:08:29 +0800

    update: server and route setup

--- server.js
+++ server.js
@@ -16,21 +16,8 @@ db.once('open', () => console.log('Connected to Database'))
 app.use(cors())
 app.use(express.json())
 
-// const brandsRouter = require('./routes/brands')
-// const searchRouter = require('./routes/search')
-// const vehicleRouter = require('./routes/vehicles')
-// const inquiriesRouter = require('./routes/inquiries')
-const usersRouter = require('./routes/users')
-// const sampleRouter = require('./routes/samples')
+const USERS_ROUTER = require('./routes/users')
 
-// app.use('/brands', brandsRouter)
-// app.use('/search', searchRouter)
-// app.use('/vehicles', vehicleRouter)
-// app.use('/inquiries', inquiriesRouter)
-app.use('/users', usersRouter)
-// app.use('/samples', sampleRouter)
-
-// app.use('/images', express.static('images'));
-
+app.use('/users', USERS_ROUTER)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
